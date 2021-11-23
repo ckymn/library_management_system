1ffcda6bb378532d061d373b562c4b40f0fcc335
ckymn <cokyamanmuhammet@gmail.com>
2021-11-23 18:00:17 +0300

    first commit

---
+++ models/book_models.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"github.com/ckymn/library_manangment_system/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Book{})
+}
